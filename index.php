c46e005fc8e2e6f273b493fc9c024e36f18ac628
sghaier <sghaierskipper@gmail.com>
2017-11-28 12:34:41 +0100

    styling index page

--- index.php
+++ index.php
@@ -12,21 +12,30 @@
     echo $json_data['user']['username']; 
     ?>'s Badges
     </title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="#" rel="stylesheet">
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
+    <link rel="stylesheet" href="/css/grid.css" media="screen" title="no title" charset="utf-8">
+<title>Badger</title>
   </head>
   <body>
-    <?php
-    
-      foreach ($courses as $course) {
-        echo '<div>';
-        echo '<img src="'. $course["badge"].'">';
-        echo '<a href="'. $course['url'].'">'.$course['title'].'</a>';
-        echo '</div>';
-        
-      }
-    
-    ?>
+  <header>
+    <h1>Badger</h1>
+  </header>
+    <div class="container">
+      <h2>courses User has completed</h2>
+      <div class="grid">
+      <?php
+      
+        foreach ($courses as $course) {
+          echo '<div class="grid-cell">';
+          echo '<img height="200px" src="'. $course["badge"].'"></br>';
+          echo '<a href="'. $course['url'].'">'.$course['title'].'</a>';
+          echo '</div>';
+          
+        }
+      
+      ?>
+      </div>
+    </div>
   </body>
 </html>
